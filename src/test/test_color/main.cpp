88d7be91989c25877dc754a1ac710e31ac8afcbd
Paul Raffer <paul.raffer@protonmail.com>
2020-02-28 17:19:21 +0100

    operator-*/

--- src/test/test_color/main.cpp
+++ src/test/test_color/main.cpp
@@ -3,12 +3,18 @@
 #include "../../lib/experimental/color/color.hpp"
 #include "../../lib/type/type.hpp"
 
-using namespace std::literals;
+
 
 int main()
 {
-    constexpr auto color = Color{255, 4, 6};
+    using namespace std::literals;
+    using namespace raffer;
+
+    constexpr auto color1 = Color{255, 4, 6};
     constexpr auto color2 = Color{7};
 
-    std::cout << color + color2 + white << std::endl;
+    std::complex c = 6i + std::complex{7.};
+
+    std::cin >> c;
+    std::cout << c << std::endl;
 }
