ac0bfe6b2f5245c8a31e025d5c9deeafa644d7be
Paul Raffer <paul.raffer@protonmail.com>
2020-02-28 18:29:39 +0100

    color not experimental

---
+++ src/lib/experimental/system/system.cpp
@@ -0,0 +1,15 @@
+#include "system.hpp"
+
+#include <cstdlib>
+
+auto clear_screen() -> int
+{
+    #if defined(__linux__) || (defined(__APPLE__) && defined(__MACH__))
+        return system("clear");
+    #elif defined(_WIN32)
+        return system("cls");
+    #else
+        #error "OS not supported!"
+    #endif
+}
+
