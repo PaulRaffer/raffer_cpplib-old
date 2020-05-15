70eb4ee0486bd8598c94e90d4f70455b35b12a90
Paul Raffer <paul.raffer@protonmail.com>
2020-05-15 13:33:51 +0200

    added class fraction and default comparison operators

---
+++ src/include/raffer/operators/arithmetic_operators.hpp
@@ -0,0 +1,25 @@
+#ifndef RAFFER_OPERATORS_ARITHMETIC_OPERATORS_HPP
+#define RAFFER_OPERATORS_ARITHMETIC_OPERATORS_HPP
+
+#include <utility>
+
+namespace raffer // interface
+{
+
+
+
+
+} // namespace raffer
+
+
+
+
+
+namespace raffer // implementation
+{
+
+
+
+} // namespace raffer
+
+#endif // RAFFER_OPERATORS_ARITHMETIC_OPERATORS_HPP
