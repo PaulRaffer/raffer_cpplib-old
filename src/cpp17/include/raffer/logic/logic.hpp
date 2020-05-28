d97aa254e66f68102895762584b61608dd127fb2
Paul Raffer <paul.raffer@protonmail.com>
2020-05-28 21:31:36 +0200

    src/cpp14/test/math/test1/build/

--- src/cpp17/include/raffer/logic/logic.hpp
+++ src/cpp17/include/raffer/logic/logic.hpp
@@ -9,15 +9,15 @@
 namespace raffer // interface
 {
 
-template <typename T> auto logical_not(T && x) -> bool;
-template <typename... T> auto logical_and(T && ...x) -> bool;
-template <typename... T> auto logical_or(T && ...x) -> bool;
-template <typename... T> auto logical_nand(T && ...x) -> bool;
-template <typename... T> auto logical_nor(T && ...x) -> bool;
-template <typename... T> auto logical_xor(T && ...x) -> bool;
-template <typename... T> auto logical_xnor(T && ...x) -> bool;
+template <typename T> auto logical_not(T && x);
+template <typename... T> auto logical_and(T && ...x);
+template <typename... T> auto logical_or(T && ...x);
+template <typename... T> auto logical_nand(T && ...x);
+template <typename... T> auto logical_nor(T && ...x);
+template <typename... T> auto logical_xor(T && ...x);
+template <typename... T> auto logical_xnor(T && ...x);
 
-template <typename T> auto bit_not(T && x) -> T;
+template <typename T> auto bit_not(T && x);
 template <typename... T> auto bit_and(T && ...x);
 template <typename... T> auto bit_or(T && ...x);
 template <typename... T> auto bit_nand(T && ...x);
@@ -33,13 +33,13 @@ template <typename... T> auto bit_xnor(T && ...x);
 namespace raffer // implementation
 {
 
-template <typename T> auto logical_not(T && x) -> bool { return !std::forward<T>(x); }
-template <typename... T> auto logical_and(T && ...x) -> bool { return (... && std::forward<T>(x)); }
-template <typename... T> auto logical_or(T && ...x) -> bool { return (... || std::forward<T>(x)); }
-template <typename... T> auto logical_nand(T && ...x) -> bool { return logical_not(logical_and(std::forward<T>(x)...)); }
-template <typename... T> auto logical_nor(T && ...x) -> bool { return logical_not(logical_or(std::forward<T>(x)...)); }
+template <typename T> auto logical_not(T && x) { return !std::forward<T>(x); }
+template <typename... T> auto logical_and(T && ...x) { return (... && std::forward<T>(x)); }
+template <typename... T> auto logical_or(T && ...x) { return (... || std::forward<T>(x)); }
+template <typename... T> auto logical_nand(T && ...x) { return logical_not(logical_and(std::forward<T>(x)...)); }
+template <typename... T> auto logical_nor(T && ...x) { return logical_not(logical_or(std::forward<T>(x)...)); }
 
-template <typename T> auto bit_not(T && x) -> T { return ~std::forward<T>(x); }
+template <typename T> auto bit_not(T && x) { return ~std::forward<T>(x); }
 template <typename... T> auto bit_and(T && ...x) { return (... & std::forward<T>(x)); }
 template <typename... T> auto bit_or(T && ...x) { return (... | std::forward<T>(x)); }
 template <typename... T> auto bit_nand(T && ...x) { return bit_not(bit_and(std::forward<T>(x)...)); }
