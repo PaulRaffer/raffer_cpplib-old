1370c9a5f3b1cb0172b482b70066c8232d2233a4
Paul Raffer <paul.raffer@protonmail.com>
2020-07-16 16:18:53 +0200

    added license to source and header files

--- cpp17/include/raffer/regex/call/call_std.hpp
+++ cpp17/include/raffer/regex/call/call_std.hpp
@@ -1,3 +1,9 @@
+
+// Copyright (c) 2020 Paul Raffer.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+
 #if __cplusplus >= 201703L
 
 #ifndef RAFFER_REGEX_CALL_CALL_STD_HPP
